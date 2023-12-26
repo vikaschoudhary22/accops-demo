7149158fc2a45a1560a711d62297c007206b7c9f
vikas <vikasdudi22@gmail.com>
2023-12-26 11:01:26 +0530

    assignment3

---
+++ assignment3/animal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace assignment3
+{
+    // Base class
+    class Animal
+    {
+        // Method to make a sound (virtual method can be overridden in derived classes)
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic animal sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        // Override MakeSound method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof! Woof!");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+        // Override MakeSound method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow! Meow!");
+        }
+    }
+
+    // Derived class Bird
+    class Bird : Animal
+    {
+        // Override MakeSound method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Chirp! Chirp!");
+        }
+    }
+
+    class animal
+    {
+        static void Main()
+        {
+            // Create objects of derived classes
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+            Animal bird = new Bird();
+
+            // Call the MakeSound method for each object
+            Console.WriteLine("Dog sounds:");
+            dog.MakeSound();
+
+            Console.WriteLine("\nCat sounds:");
+            cat.MakeSound();
+
+            Console.WriteLine("\nBird sounds:");
+            bird.MakeSound();
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignment3/shape.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace assignment3_shape
+{
+    // Base class
+    class Shape
+    {
+        // Method to calculate the area (virtual method can be overridden in derived classes)
+        public virtual double CalculateArea()
+        {
+            return 0;
+        }
+    }
+
+    // Derived class Rectangle
+    class Rectangle : Shape
+    {
+        // Properties
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        // Constructor
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        // Override CalculateArea method
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+
+    // Derived class Circle
+    class Circle : Shape
+    {
+        // Property
+        public double Radius { get; set; }
+
+        // Constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Override CalculateArea method
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Create objects of derived classes
+            Rectangle rectangle = new Rectangle(5, 10);
+            Circle circle = new Circle(7);
+
+            // Display the areas of the shapes
+            Console.WriteLine("Area of Rectangle: " + rectangle.CalculateArea());
+            Console.WriteLine("Area of Circle: " + circle.CalculateArea());
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
